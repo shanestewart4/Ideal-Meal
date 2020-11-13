ca48cb7795a3d25347f1be40779a4580e3614477
shanestewart4 <shanestewartguitar@gmail.com>
2020-11-12 18:27:14 -0600

    initial recipe js, html organizations, css updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,29 @@
-var foodInputE1 = document.querySelector("#food-text");
-var locationInput = document.querySelector("#location-input");
+// Input Elements
+let foodInput = document.querySelector("#food-text");
+let locationInput = document.querySelector("#location-input");
 
+// button 
+let recipeButton = document.getElementById("food-submit");
+
+// EDAMAM Recipe Search API keys & app ID
+
+
+// Add event listener to the submit button that calls the API request when clicked.
+recipeButton.addEventListener("click", () => {
+    console.log("food entered")
+    recipeRequest();
+});
+
+
+// RECIPE SEARCH FUNCTION
+
+// async function to fetch recipe data from API
+async function recipeRequest () {
+    let apiKeyRecipe = "b08658ce75bfb8d1774340639ff3f1bf";
+    let edamamAppID = "89dfaab6";
+
+    let response = await fetch(``);
+    console.log(response)
+}
+
+// function to display the name, image, ingredients and 
